9780573012d01bee2be7f8c2e73b2ca0dbc7ee57
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2021-01-15 16:05:25 +0700

    Update db.php

--- system/db.php
+++ system/db.php
@@ -1,7 +1,7 @@
 <?php
-$database["host"] = "{{host}}";
-$database["name"] = "{{name}}";
-$database["user"] = "{{user}}";
-$database["password"] = "{{pass}}";
+$database["host"] = "localhost";
+$database["name"] = "allsosmed1234";
+$database["user"] = "allsosmed1234";
+$database["password"] = "allsosmed1234";
 require_once __DIR__ . "/../admin/classes/database.class.php";
 database::connect("mysql:host=" . $database["host"] . ";dbname=" . $database["name"] . ";charset=utf8mb4", $database["user"], $database["password"]);
